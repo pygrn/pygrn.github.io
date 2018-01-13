388f11ebc1f2f1ad7a970619f38d47874b5d3be4
Xavi Torelló <info@xaviertorello.cat>
2018-01-13 11:39:00 +0100

    Add pelican base config

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Xavi Torelló'
+SITENAME = 'Python Girona'
+SITEURL = 'https://pygrn.github.io/'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Andorra'
+
+DEFAULT_LANG = 'ca'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = (('Meetup', 'https://www.meetup.com/PythonGirona'),
+          ('Github', 'https://github.com/pygrn'),)
+
+DEFAULT_PAGINATION = 3
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
